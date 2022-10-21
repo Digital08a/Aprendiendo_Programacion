e8a7a31d93f551b12d6625340257218c8f5b9464
Alexis <alexx_d_a@hotmail.com>
2022-10-21 19:15:00 -0300

    correccion de errores
    se corrigen bucle infinito en function calcular promedio, y se corrige funcion obtener menor y mayor

--- tareas/clase05/tarea04/script.js
+++ tareas/clase05/tarea04/script.js
@@ -17,70 +17,56 @@ pre-creado con el texto "El número más frecuente es..."
     const numeroMayor = document.querySelectorAll("#numero-mayor");
     const masFrecuente = document.querySelectorAll("#mas-frecuente");
 */
-const botonCalcular = document.querySelector("#calcular");
 
-botonCalcular.onclick = function () {
+function sumaTotal() {
     const listaDeNumeros = document.querySelectorAll("li");
     var arrayNumeros = [];
 
     for (let i = 0; i < listaDeNumeros.length; i++) {
         arrayNumeros.push(Number(listaDeNumeros[i].textContent));
     }
-
-    function calcularPromedio() {
-        let sumaDeNumeros = 0;
-        for (let i = 0; arrayNumeros.length; i++) {
-            sumaDeNumeros += arrayNumeros[i];
-        }
-        let promedio = sumaDeNumeros / arrayNumeros.length;
-        console.log(promedio);
-        return promedio;
-    }
-
     function obtenerNumeroMenor() {
         let numeroMenor = arrayNumeros[0];
-        for (let i = 0; i < arrayNumeros; i++) {
+        for (let i = 0; i < arrayNumeros.length; i++) {
             if (numeroMenor > arrayNumeros[i]) {
                 numeroMenor = arrayNumeros[i];
             }
         }
-        console.log(numeroMenor);
         return numeroMenor;
     }
     function obtenerNumeroMayor() {
         let numeroMayor = arrayNumeros[0];
-        for (let i = 0; i < arrayNumeros; i++) {
+        for (let i = 0; i < arrayNumeros.length; i++) {
             if (numeroMayor < arrayNumeros[i]) {
                 numeroMayor = arrayNumeros[i];
             }
         }
-        console.log(numeroMayor);
         return numeroMayor;
     }
-
     function obtenerNumeroRepetido() {
-        for (let i = 0; i < arrayNumeros; i++) {
-            let auxiliar = arrayNumeros[0];
+        let auxiliar = arrayNumeros[0];
+        for (let i = 0; i < arrayNumeros.length; i++) {
             if (auxiliar > arrayNumeros[i]) {
                 auxiliar = arrayNumeros[i];
             }
         }
-        console.log(auxiliar);
         return auxiliar;
     }
-    
-    function mostrarDatos(){
+
+    function calcularPromedio() {
+        let sumaDeNumeros = 0;
+        for (let i = 0; i < arrayNumeros.length; i++) {
+            sumaDeNumeros += arrayNumeros[i];
+        }
+        let promedio = sumaDeNumeros / arrayNumeros.length;
+        return promedio;
+    }
+    function mostrarDatos() {
         document.getElementById("promedio").innerHTML = "El promedio es : " + calcularPromedio();
         document.getElementById("numero-menor").innerHTML = "El número más pequeño es : " + obtenerNumeroMenor();
         document.getElementById("numero-mayor").innerHTML = "El número más grande es : " + obtenerNumeroMayor();
-        document.getElementById("mas-frecuente").innerHTML = "El número más frecuente es : "+ obtenerNumeroRepetido();
+        document.getElementById("mas-frecuente").innerHTML = "El número más frecuente es : " + obtenerNumeroRepetido();
     }
     mostrarDatos();
-    
-};
-// 
-/*
-for (let i = 0; i < listaDeNumeros.length; i++) {
-    console.log(arrayNumeros[i])
 }
-*/
+
